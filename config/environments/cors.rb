34d6d2a1689e8885d6863f9bda30d7740bdce0d8
rachityadav <rachit.r.yadav@gmail.com>
2022-05-02 00:25:19 +0530

    new changes

--- config/environments/cors.rb
+++ config/environments/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001'
+      origins 'https://626ed32d276ce204132cfa5b--amazing-sunshine-8d59e1.netlify.app/'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put]
     end
   end
