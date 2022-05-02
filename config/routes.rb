316f239c40b291a9fff27cac92c4e95e972a5093
rachityadav <rachit.r.yadav@gmail.com>
2022-05-02 08:23:53 +0530

    status added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  match '*path', via: [:options], to: lambda {|_| [204, {'Content-Type' => 'text/plain'}]}
   get '/search_phone_number', to: 'pages#search_contact' 
   post '/add_new_contact', to: 'pages#add_new_contact'
   # Defines the root path route ("/")
